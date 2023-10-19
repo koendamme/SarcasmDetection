bb419cf6c891993eb9b43cd48a1703718db8ee85
s1992988 <m.m.c.romeijn@student.utwente.nl>
2023-10-19 14:56:14 +0200

    Added evaluation set and started with using the evaluation set. This does not work yet.

--- training.py
+++ training.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import torchsummary as torchsummary
+
 from sarcasm_dataset import SarcasmDataset
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -16,18 +18,23 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Loaded device: {device}")
 
-    dataset = SarcasmDataset("data.json", "word2vec/word2vec.model")
+    dataset = SarcasmDataset("train_data.json", "word2vec/word2vec.model")
+    dataset_val = SarcasmDataset("val_data.json", "word2vec/word2vec.model")
     loader = DataLoader(dataset, batch_size=16)
+    loader_val = DataLoader(dataset_val, batch_size=16)
+
+    val_losses = []
 
     n_epochs = 10
     embedding_size = 100
     model = NeuralNetwork(embedding_size)
     model = model.to(device)
-    loss = F.binary_cross_entropy
+    # loss = F.binary_cross_entropy
+    bce = nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=.001)
 
     for epoch in range(n_epochs):
-        model.train()
+        model.train(True)
         train_loss = 0
         step = 0
 
@@ -35,17 +42,40 @@ if __name__ == '__main__':
         for x_batch, y_batch in tqdm(loader):
             x_batch = x_batch.to(device)
             y_batch = y_batch.to(device)
+            optimizer.zero_grad()
 
             output = model(x_batch)
-
             output = torch.round(output)
-            loss = F.binary_cross_entropy(torch.round(output), y_batch)
+            loss = bce(torch.round(output), y_batch)
 
             # Backpropagation
-            optimizer.zero_grad()
+
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
             step += 1
 
         train_loss /= step
+        # print(output)
+        # model.eval()
+        # val_loss = 0
+        # step = 0
+        # print("Validating...")
+        # for x_batch, y_batch in tqdm(loader_val):
+        #     x_batch = x_batch.to(device)
+        #     y_batch = y_batch.to(device)
+        #
+        #     output = model(x_batch)
+        #
+        #     output = torch.round(output)
+        #     loss = F.binary_cross_entropy(torch.round(output), y_batch)
+        #     val_loss += loss.item()
+        #     step += 1
+        # val_losses.append(val_loss / step)
+        # print("validation loss:", val_loss/step)
+        print("training loss:", train_loss)
+
+
+
+
+# print(val_losses)

e603a61bbe6c00e40454144b83b5d0af0f21ad29
koendamme <koen.damme1999@gmail.com>
2023-10-23 18:26:51 +0200

    geen idee

--- embeddings.py
+++ embeddings.py
@@ -6,7 +6,7 @@ def word2vec(data_path, save_dir, embedding_size=100):
     with open(data_path, 'r') as f:
         d = json.load(f)
 
-    w2v = Word2Vec(sentences=d["data"], min_count=1, vector_size=embedding_size)
+    w2v = Word2Vec(sentences=d["data"], vector_size=embedding_size)
     w2v.save(save_dir)
 
 
